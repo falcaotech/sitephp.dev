226ed6b4e01574a16d0b0dda73d354c5ff911d44
Neto <antoniofalcaoneto@gmail.com>
2014-12-11 14:35:53 -0200

    limpando o código

--- index.php
+++ index.php
@@ -4,20 +4,8 @@
         <div class="row">
             <?php
             
-                // Todas as informaçoes do servidor e da URL
-                //print_r($_SERVER);
-
                 // Pegando a URL completa
                 $rota = parse_url('http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-                
-                // Parte da URL que vem depois do dominio (HTTP_HOST)
-                //$uri = $_SERVER['REQUEST_URI'];
-                
-                // Retirando a primeira barra da URI
-                //$uri = substr($uri, 1, strlen($uri)); 
-                
-                // Quebrando a URI em um array
-                //$parametros = explode('/', $uri);
                
                 //verifico se existe uma baseUrl a ser utilizada
                 $posBaseUrl = strpos($rota['path'], $baseUrl);
@@ -39,12 +27,3 @@
         </div>
     </div>
     <?php require_once 'footer.php';?>
-
-<!--Esse é o código que ele usa no vídeo, mais diz que não é a maneira 100% correta de se fazer, então sugere que os alunos achem uma maneira melhor de utilizar isso
-<!--?php $rota = parse_url("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-$path = str_replace("/","",$rota['path']);?>
-
-//Dentro do conteúdo
-<!--<div>
-    <!--?php require_once($path.".php"); ?>
-</div>-->

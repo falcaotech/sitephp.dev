3a475e02d1961cbf74faa1ad203b0d9f7bd60cb9
Neto <antoniofalcaoneto@gmail.com>
2014-12-18 15:57:10 -0200

    Alterações ajustando as rotas

--- index.php
+++ index.php
@@ -3,29 +3,43 @@
     <div class="span10 conteudo">
         <div class="row">
             <?php
-                
-                // Pegando a URL completa
-                $rota = parse_url('http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-                                              
-                //caso contrario utilizo a requisição passada.
-                $pageRequest = (String)trim($rota['path'], "/");
-                
-                // Quebrando a $pageRequest em um array
-                $parametros = explode('/', $pageRequest);
-                
-                // Guarda o primeiro parametro na variavel pagina
-                $pagina = (isset($parametros[0]) && !empty($parametros[0])) ? $parametros[0] : 'home';
-                
-                // Verifica se o arquivos referente a pagina existe
-                if (file_exists($pagina . '.php')) {
-                    // Se existir, inclui o arquivo
-                    require_once $pagina . '.php';
-                } else {
-                    // Se no existir, inclui o 404.php
-                    require_once '404.php';
+                function pegaConteudo()
+                {
+                    // Pegando a URL completa
+                    $rota = parse_url('http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+
+                    //caso contrario utilizo a requisição passada.
+                    $pageRequest = (String)trim($rota['path'], "/");
+
+                    // Quebrando a $pageRequest em um array
+                    $parametros = explode('/', $pageRequest);
+
+                    // guarda na variavel pagina o primeiro parametro acrescentado do '.php'
+                    $pagina = (isset($parametros[0]) && !empty($parametros[0])) ? $parametros[0].'.php' : 'home.php';
+                    
+                    // Cria um arra com os arquivos possveis de serem carregados
+                    $rotas = [
+                        'home.php',
+                        'empresa.php',
+                        'produtos.php',
+                        'servicos.php',
+                        'contato.php',
+                    ];
+                    
+                    // Verifica se a variavel pagina est no array definido
+                    if (in_array($pagina, $rotas)) {
+                        // Se estiver, inclui o arquivo
+                        require_once $pagina;
+                    } else {
+                        // Se não estiver, inclui o 404.php
+                        require_once '404.php';
+                    }
                 }
+                // Executa a funço pegaConteudo 
+                pegaConteudo();
                 
             ?>
         </div>
     </div>
+
     <?php require_once 'footer.php';?>
